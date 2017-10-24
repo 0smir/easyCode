0dc80cca05e30b36fb54fd16c6aa2ec9953f622d
Olga Smirnova <o.smirnova@soft-industry.com>
2017-10-24 10:29:53 +0300

    add function console.log odd numbers

--- HW3/js/index.js
+++ HW3/js/index.js
@@ -2,11 +2,12 @@
 var string = 'I am in the easycode';
 var newString = '';
 var i = 0;
-for( ; i < string.length; i++){
+
+for( ; i < string.length; i++) {
     if(string[i] === ' '){
         newString = newString + string[i] + string[i + 1].toUpperCase();
         i += 1;
-    }else{
+    } else {
         newString = newString + string[i];
     }
 }
@@ -16,8 +17,8 @@ document.write(newString + '<br>');
 let strRtl = 'tseb eht ma i';
 let strLtr = '';
 let iterCounter = strRtl.length;
-console.log(iterCounter);
-while(iterCounter > 0){
+
+while(iterCounter > 0) {
     strLtr += strRtl[iterCounter - 1];
     iterCounter--;
 }
@@ -28,7 +29,8 @@ document.write(strLtr + '<br>');
 
 let digit = 10;
 let accum = 1;
-while(digit > 0){
+
+while(digit > 0) {
     accum = accum * (digit);
     digit--;
 }
@@ -37,13 +39,13 @@ document.write('factorial of 10: ', accum + '<br>');
 //Создаем строку счет до 10
 let count = 1;
 let strCountTill10 = 'Считаем до 10: ';
+
 do{
     strCountTill10 += count;
     if(count != 10){
         strCountTill10 += ', ';
     }
     count++;
-    console.log(strCountTill10);
 } while(10 >= count);
 
 document.write(strCountTill10 + '<br>');
@@ -52,17 +54,24 @@ document.write(strCountTill10 + '<br>');
 let strJS = "JavaScript is pretty good language";
 let strCamelCase = '';
 let j = 0;
-for(; j < strJS.length; j++){
+
+for(; j < strJS.length; j++) {
     if(strJS[j] === ' '){
         strCamelCase += strJS[j + 1].toUpperCase();
         j++;
-    } else{
+    } else {
         strCamelCase += strJS[j];
     }
 }
 document.write(strCamelCase);
 
 
+//Найти все нечетные числа от 1 до 15 и вывести в консоль
+let number = 15;
 
-
-
+while(number != false) {
+    if((number % 2) > 0) {
+        console.log('odd number: ', number);
+    }
+    number--;
+}
