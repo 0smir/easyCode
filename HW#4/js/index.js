bc2a6b716759d75fa56741f89d2f1edda6605a81
Olga Smirnova <o.smirnova@soft-industry.com>
2017-10-26 13:49:23 +0300

    aff function that return chart of symbol from string, add function tat console.log every simbol of string with recursion

--- HW#4/js/index.js
+++ HW#4/js/index.js
@@ -1,11 +1,10 @@
 //#1 принимает любое к-во чисел и возвращает их произведение
 function miltiply() {
-    console.log(typeof  arguments);
     let accum = 1;
     for(let i = 0; i < arguments.length; i++){
         accum *= arguments[i];
     }
-    console.log(accum);
+    console.log("miltiply function result: ", accum);
 
     return accum;
 }
@@ -15,15 +14,64 @@ miltiply(1, 2, 3, 4, 5);
 function factorial(digit) {
     let iter = function(counter, total) {
         if(counter === 1){
-            console.log(total);
+            console.log("factorial of 10: ", total);
             return total;
         } else {
+
             return  iter(counter - 1, total * counter);
         }
     };
+
     return iter(digit, 1);
-
-
 }
 
-factorial(3);
+factorial(10);
+
+//#3 принимает строку возвращает строку-перевертыш
+
+function revertString(string) {
+    let revertStr = '';
+    for(let i = 1, max =  string.length; i <= max; i++){
+        revertStr += string[string.length - i];
+    }
+    console.log("revert string: ", revertStr);
+    return revertStr;
+}
+
+revertString('test');
+
+//#4 Сщздать функцию, которая  вкачестве аргумента принимаетстроку
+//из букв и возвращает строку, где каждый символ разделен пробелом
+//и заменен на юни-код значение символа:
+
+function getCodeStringFromText(string) {
+    let codeString = '';
+
+    for(let i = 0; i < string.length; i++){
+        codeString  +=  (i !== string.length - 1) ? string.charCodeAt(i) + ' ' : string.charCodeAt(i);
+    }
+
+    return codeString;
+}
+
+console.log(getCodeStringFromText('hello'));
+
+//#5 ф-я - рекурсмия которая выводит каждый символ строки в консоль
+
+function getSymbolInLine(str) {
+    let index = 0;
+    let iter = function (str) {
+        console.log(str[index]);
+        while(index !== (str.length - 1)){
+            index++;
+            iter(str);
+        }
+    };
+
+    iter(str);
+}
+
+getSymbolInLine('test');
+
+
+
