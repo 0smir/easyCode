ac39e8d9174e1be01848a75092f4721c77a77630
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-11-05 10:30:55 +0200

    add hw4 first two tasks

---
+++ CW4/index.js
@@ -0,0 +1,16 @@
+//arrow function
+
+let arrow = (a, b) => {
+    a *= 3;
+    b *= 2;
+    return a + b;
+}
+
+let result = arrow(3, 2);
+console.log("arrow ", result);
+
+let arrowTwo = x => x * 2;
+result = arrowTwo(3);
+console.log("arrowTwo ", result);
+
+//Date
