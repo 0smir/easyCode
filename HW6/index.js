b3b73968b8958ec3a14a5dcd0c298599a5abfd3e
Olga Smirnova <o.smirnova@soft-industry.com>
2017-11-06 11:21:56 +0200

    add hw6, work with array methods

--- HW6/index.js
+++ HW6/index.js
@@ -121,10 +121,114 @@ console.log(stringArr);
 
 
 //8 сортировка массивов по длинне
+//
+// let arrayA = [1, 2, 3], arrayB = [1, 2, 3, 4], arraeC = ["o", "j", "f", 6, 5];
 
-let arrayA = [1, 2, 3], arrayB = [1, 2, 3, 4], arraeC = ["o", "j", "f", 6, 5];
-
-let bigArray = [[1, 2, 3], [1, 2, 3, 4], ["o", "j", "f", 6, 5]];
-let sortArray = () =>{
-
+let bigArray = [[1, 2, 3], [1, 2, 3, 4], ["o", "j", "f", 6, 5], [1, 2], ["o", "j", "f", 6, 5, 1, 2]];
+let sortArrayByLength = (arr) =>{
+    arr.sort(function (prev, next){
+        if(prev.length < next.length) return -1;
+        if(prev.length > next.length) return 1;
+    });
+    return arr;
 }
+
+let sortedArray = sortArrayByLength(bigArray);
+console.log("сортировка массивов по длинне", sortedArray);
+
+//9 создвть копию произвольного массива
+let getCopyOfArray = (arr) =>{
+    let arrCopy = [].concat(arr);
+    return arrCopy;
+}
+
+let copyOfArryResult = getCopyOfArray(bigArray);
+console.log("arr Copy with concat", copyOfArryResult);
+
+let getArrayCopyWithSlice = (arr) =>{
+    let arrayCopySplice = arr.slice();
+    return arrayCopySplice;
+}
+
+console.log("getArrayCopyWithSlice", getArrayCopyWithSlice(stringArr));
+
+//10 отсортировать массив объектов по возрастающему кол-ву ядер
+
+let parametrArr = [
+    {
+        cpu: 'intel',
+        info: {
+            cores: 2,
+            cache: 3
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 4,
+            cache: 4
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 1,
+            cache: 1
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 3,
+            cache: 2
+        }
+    },
+    {
+        cpu: 'intel',
+        info: {
+            cores: 4,
+            cache: 2
+        }
+    }
+];
+
+let sortByCoreCount = (arr) =>{
+    arr.sort(function (prev, next) {
+        if(prev.info.cores < next.info.cores) return -1;
+        if(prev.info.cores > next.info.cores) return 1;
+    });
+    return arr;
+}
+
+console.log("массив объектов c возрастающему кол-вом ядер", sortByCoreCount(parametrArr));
+
+//11 Создать ф-ю, которая будет принимать массив продуктов и две цены. Ф-я должна вернуть все прод-ты, цена которых
+// находиться в указанном диапазоне, и отсортировать от дешевых к дорогим.
+
+let products = [
+    {title: "prod1", price: 5.2},
+    {title: "prod2", price: 0.18},
+    {title: "prod3", price: 15},
+    {title: "prod4", price: 25},
+    {title: "prod5", price: 18.9},
+    {title: "prod6", price: 8},
+    {title: "prod7", price: 19},
+    {title: "prod8", price: 63}
+];
+
+let priceFilter = (arr, from, till) =>{
+    let prodactRange = [];
+    arr.sort(function (prev, next) {
+        if(prev.price < next.price) return -1;
+        if(prev.price > next.price) return 1;
+    });
+    for(let i = 0; i < arr.length; i++){
+        if((from <= arr[i].price) && (arr[i].price <= till)){
+            prodactRange.splice( prodactRange.length, 0, arr[i]);
+        }
+    }
+    return prodactRange;
+}
+
+let priceFilterResult = priceFilter(products, 0, 10);
+console.log("prodactRange", priceFilterResult);
